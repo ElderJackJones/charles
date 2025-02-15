a0886dc3c4442e26eb093261af6dc9df68de90f3
Charleston SML <jpjones@missionary.org>
2025-02-14 21:28:48 -0500

    get charles working, still needs correct end message once everything's sent and a brain

--- referral_list-master/src/holly/mod.rs
+++ referral_list-master/src/holly/mod.rs
@@ -36,90 +36,107 @@ pub async fn main(church_client: &mut ChurchClient) -> anyhow::Result<()> {
     let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
     tokio::task::spawn_blocking(move || user_input_loop(tx));
 
-    loop {
-        let mut stream = tokio::net::TcpStream::connect(&holly_config.holly_socket).await?;
-        let mut next_time_check = tokio::time::Instant::now() + tokio::time::Duration::from_secs(1);
-        if loop {
-            let mut buf = [0u8; 1024 * 8];
-            tokio::select! {
-                written = stream.read(&mut buf) => {
-                    let written = match written {
-                        Ok(w) => w,
-                        Err(e) => match e.kind() {
-                            std::io::ErrorKind::WouldBlock => {
-                                continue;
-                            }
-                            _ => {
-                                error!("Error receiving data from Holly! {e:?}");
-                                break false;
-                            }
-                        },
-                    };
-                    if written == 0 {
-                        error!("Holly stopped sending data!");
-                        break false;
-                    }
+    // Connect to Holly only once.
+    let mut stream = tokio::net::TcpStream::connect(&holly_config.holly_socket).await?;
+    let next_time_check = tokio::time::Instant::now() + tokio::time::Duration::from_secs(1);
+    let mut buf = [0u8; 1024 * 8];
 
-                    if let Ok(payload) = String::from_utf8(buf[0..written].to_vec()) {
-                        if let Ok(payload) = serde_json::from_str::<Message>(&payload) {
-                            info!("Recieved message from Holly: {payload:?}");
-                        }
+    tokio::select! {
+        // Option 1: Data received from Holly.
+        written = stream.read(&mut buf) => {
+            let written = match written {
+                Ok(w) => w,
+                Err(e) => {
+                    error!("Error receiving data from Holly! {e:?}");
+                    return Ok(());
+                },
+            };
+            if written == 0 {
+                error!("Holly stopped sending data!");
+                return Ok(());
+            }
+
+            if let Ok(payload) = String::from_utf8(buf[0..written].to_vec()) {
+                if let Ok(payload) = serde_json::from_str::<Message>(&payload) {
+                    info!("Received message from Holly: {payload:?}");
+                }
+            } else {
+                error!("Received a non-utf8 vector from Holly");
+                return Ok(());
+            }
+        }
+        // Option 2: Time to check and possibly send Holly's list.
+        _ = sleep_until(next_time_check) => {
+            info!("Checking if it's time to send Holly's list");
+            let mut st = send_time::SendTime::load(&church_client.env).await?;
+            println!("about to check if go time");
+            println!("{}", st.has_gone);
+            if !st.has_gone {
+                info!("Sending Holly's list!");
+                println!("not true, sending");
+                st.gone().await?;
+                let report = if let Some(report) = crate::report::Report::read_report(&church_client.env)? {
+                    report
+                } else {
+                    crate::generate_report(church_client).await?
+                };
+
+                let contacts = crate::get_average(church_client).await?;
+                let mut contacts = contacts.into_iter().collect::<Vec<(String, usize)>>();
+                contacts.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+
+                fn format_contact_time(total_minutes: usize) -> String {
+                    if total_minutes < 60 {
+                        format!("{}min", total_minutes)
                     } else {
-                        error!("Recieved a non-utf8 vector from Holly");
-                        break false;
+                        let hours = total_minutes / 60;
+                        let minutes = total_minutes % 60;
+                        format!("{}hr {}min", hours, minutes)
                     }
                 }
-                _ = sleep_until(next_time_check) => {
-                    info!("Checking if it's time to send Holly's list");
-                    next_time_check = tokio::time::Instant::now() + tokio::time::Duration::from_secs(20);
-                    let mut st = send_time::SendTime::load(&church_client.env).await?;
-                    if st.is_go_time().await? {
-                        info!("Sending Holly's list!");
-                        let report = if let Some(report) = crate::report::Report::read_report(&church_client.env)? {
-                            report
-                        } else {
-                            crate::generate_report(church_client).await?
-                        };
+                
 
-                        let contacts = crate::get_average(church_client).await?;
-                        let mut contacts = contacts.into_iter().collect::<Vec<(String, usize)>>();
-                        contacts.sort_unstable_by(|a, b| a.1.cmp(&b.1));
-
-                        let mut avg_report = "".to_string();
-                        for (k, v) in contacts {
-                            if let Some(bl) = &holly_config.blacklist {
-                                if bl.contains(&k) {
-                                    continue;
-                                }
-                            }
-                            avg_report = format!("{avg_report}\n{k}: {v}");
-                        }
-                        for (zone_id, chat_id) in &holly_config.zone_chats {
-                            let msg = if let Some(p) = report.get_pretty_zone(zone_id) {
-                                format!("Average Contact Time:\n{avg_report}\n\nUncontacted Referrals:\n\n{p}")
-                            } else {
-                                info!("No uncontacted referrals in {zone_id}");
-                                format!("Average Contact Time:{avg_report}\n\nNo uncontacted referrals! Great work!")
-                            };
-                            info!("Sending {msg} to {chat_id}");
-                            stream.write_all(&Message { content: msg, chat_id: chat_id.to_string(), ..Default::default() }.to_bytes()).await?;
-                        }
-                        if let Some(chat_id) = &holly_config.unassigned_chat {
-                            let msg = report.unassigned.join("\n");
-                            info!("Sending {msg} to {chat_id}");
-                            stream.write_all(&Message { content: msg, chat_id: chat_id.to_string(), ..Default::default() }.to_bytes()).await?;
+                let mut avg_report = String::new();
+                for (k, v) in contacts {
+                    if let Some(bl) = &holly_config.blacklist {
+                        if bl.contains(&k) {
+                            continue;
                         }
                     }
+                    avg_report = format!("{avg_report}\n{k}: {}", format_contact_time(v));
                 }
-                _ = rx.recv() => {
-                    info!("Disconnecting from Holly...");
-                    break true;
+                for (zone_id, chat_id) in &holly_config.zone_chats {
+                    let msg = if let Some(p) = report.get_pretty_zone(zone_id) {
+                        format!("Good Morning, y'all! I hope everyone has a great day! Let's BELIEVE, and GO contact those referrals <3 \n ._.)/\\(._. \n \nAverage Contact Time:\n{avg_report}\n\n -->Uncontacted Referrals<--\n{p}")
+                    } else {
+                        info!("BOOYAH! No uncontacted referrals in {zone_id}");
+                        format!("Average Contact Time:{avg_report}\n\nNo uncontacted referrals! Great work!")
+                    };
+                    info!("Sending {msg} to {chat_id}");
+                    stream.write_all(&Message {
+                        content: msg,
+                        chat_id: chat_id.to_string(),
+                        ..Default::default()
+                    }.to_bytes()).await?;
+                }
+                if let Some(chat_id) = &holly_config.unassigned_chat {
+                    let msg = report.unassigned.join("\n");
+                    info!("Sending {msg} to {chat_id}");
+                    stream.write_all(&Message {
+                        content: msg,
+                        chat_id: chat_id.to_string(),
+                        ..Default::default()
+                    }.to_bytes()).await?;
                 }
             }
-        } {
-            break;
+            println!("All done! Type 'q' to exit.")
+        }
+        // Option 3: User requested disconnect.
+        _ = rx.recv() => {
+            info!("Disconnecting from Holly...");
         }
     }
+
     Ok(())
 }
 
@@ -127,5 +144,5 @@ fn user_input_loop(sender: UnboundedSender<()>) {
     println!("Press 'q' and then enter to disconnect from Holly gracefully.");
     let mut buf = String::new();
     let _ = std::io::stdin().read_line(&mut buf).is_ok();
-    let _ = sender.send(()).is_ok();
+    let _ = sender.send(());
 }
--- referral_list-master/src/holly/send_time.rs
+++ referral_list-master/src/holly/send_time.rs
@@ -2,9 +2,7 @@
 
 use std::{path::PathBuf, str::FromStr};
 
-use anyhow::Context;
 use chrono::{Days, NaiveDateTime, NaiveTime, Timelike};
-use rand::Rng;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
@@ -13,6 +11,8 @@ pub struct SendTime {
     pub next: NaiveDateTime,
     #[serde(skip)]
     path: PathBuf,
+    #[serde(skip)]
+    pub has_gone: bool,
 }
 
 impl SendTime {
@@ -21,6 +21,7 @@ impl SendTime {
         if !std::fs::exists(&file_path)? {
             let res = Self {
                 path: file_path,
+                has_gone: false,
                 ..Default::default()
             };
             return Ok(res);
@@ -31,58 +32,52 @@ impl SendTime {
         Ok(res)
     }
 
-    pub async fn is_go_time(&mut self) -> anyhow::Result<bool> {
-        if self.last == self.next {
-            self.set_next().await?;
-            return Ok(true);
+    pub async fn gone(&mut self) -> anyhow::Result<()> {
+        if !self.has_gone {
+            self.has_gone = true;
         }
-        let now = chrono::Local::now().naive_local();
-        if now > self.next {
-            self.last = self.next;
-            self.set_next().await?;
-            return Ok(true);
-        }
-        Ok(true)
-    }
-
-    async fn set_next(&mut self) -> anyhow::Result<()> {
-        let now = chrono::Local::now().naive_local();
-
-        // Define the start and end times
-        // We are still doing 6:30 to 12 even on the same day because this will auto fire even if the time is after.
-        let start_time = NaiveTime::from_hms_opt(6, 30, 0).unwrap(); // 6:30 AM
-        let end_time = NaiveTime::from_hms_opt(12, 0, 0).unwrap(); // 12:00 PM
-
-        // Calculate the range in minutes
-        let start_minutes = start_time.num_seconds_from_midnight() / 60;
-        let end_minutes = end_time.num_seconds_from_midnight() / 60;
-
-        // Generate a random number of minutes between the start and end
-        let random_minutes = rand::thread_rng().gen_range(start_minutes..=end_minutes);
-        let random_time = NaiveTime::from_hms_opt(random_minutes / 60, random_minutes % 60, 0);
-
-        let res = NaiveDateTime::new(
-            if self.last.date() == now.date() {
-                now.date().checked_add_days(Days::new(1)).unwrap()
-            } else {
-                now.date()
-            },
-            random_time.unwrap(),
-        );
-        self.next = res;
-        self.save().await?;
-        println!("Sending Holly's list at {}", self.next);
         Ok(())
-    }
-
-    async fn save(&self) -> anyhow::Result<()> {
-        let file = std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .truncate(true)
-            .open(&self.path)?;
-        serde_json::to_writer(file, self)
-            .context("Unable to serialize or write send time to file")?;
-        Ok(())
-    }
+    }    
 }
+
+//     async fn set_next(&mut self) -> anyhow::Result<()> {
+//         let now = chrono::Local::now().naive_local();
+
+//         // Define the start and end times
+//         // We are still doing 6:30 to 12 even on the same day because this will auto fire even if the time is after.
+//         let start_time = NaiveTime::from_hms_opt(6, 30, 0).unwrap(); // 6:30 AM
+//         let end_time = NaiveTime::from_hms_opt(12, 0, 0).unwrap(); // 12:00 PM
+
+//         // Calculate the range in minutes
+//         let start_minutes = start_time.num_seconds_from_midnight() / 60;
+//         let end_minutes = end_time.num_seconds_from_midnight() / 60;
+
+//         // Generate a random number of minutes between the start and end
+//         let random_minutes = rand::thread_rng().gen_range(start_minutes..=end_minutes);
+//         let random_time = NaiveTime::from_hms_opt(random_minutes / 60, random_minutes % 60, 0);
+
+//         let res = NaiveDateTime::new(
+//             if self.last.date() == now.date() {
+//                 now.date().checked_add_days(Days::new(1)).unwrap()
+//             } else {
+//                 now.date()
+//             },
+//             random_time.unwrap(),
+//         );
+//         self.next = res;
+//         self.save().await?;
+//         println!("Sending Holly's list at {}", self.next);
+//         Ok(())
+//     }
+
+//     async fn save(&self) -> anyhow::Result<()> {
+//         let file = std::fs::OpenOptions::new()
+//             .write(true)
+//             .create(true)
+//             .truncate(true)
+//             .open(&self.path)?;
+//         serde_json::to_writer(file, self)
+//             .context("Unable to serialize or write send time to file")?;
+//         Ok(())
+//     }
+// }
